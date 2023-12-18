2aad219972e0edc574e31f02a1e1593ae28eb286
czero <154037290+c-zero2@users.noreply.github.com>
2023-12-18 13:54:04 +0700

    Update delete-log.sh
    done

--- delete-log.sh
+++ delete-log.sh
@@ -9,3 +9,24 @@ do
 done
 # Notify the user that the script is complete
 echo "Log files deleted successfully."
+
+othe script
+
+#!/bin/bash
+# Set the path to your log files
+log_path="/path/to/your/log/files"
+# Set the threshold for log file age in days
+threshold_days=5
+# Set the output file name
+output_file="$(date +'%m-%d-%Y')_complete.txt"
+# Find and delete files older than the threshold
+find "$log_path" -type f -mtime +$threshold_days -exec rm {} \;
+# Notify completion
+echo "Deletion process complete."
+# Export information to a file
+echo "Deletion summary $(date):" > "$output_file"
+echo "Deleted files older than $threshold_days days in $log_path." >> "$output_file"
+echo "Deletion process completed on $(date)." >> "$output_file"
+
+# Notify completion
+echo "Deletion summary exported to $output_file."
